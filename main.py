f51fe0b254c6da2b695852b1ba504c595895d621
Developer <gregor.szczypek@gmail.com>
2024-05-20 21:08:40 +0200

    - implemented hysteresis

--- main.py
+++ main.py
@@ -36,10 +36,14 @@ led_time = 1
 
 # Initial states
 max_humidity = 0
+min_humidity = 0
 max_temperature = 0
+min_temperature = 0
 override_on = False
 vent_on = False
 is_manual_vent_on = False
+timeout = 900000  # 15 min timeout for reaching min threshold
+start_time = utime.ticks_ms()  # Start time for timeout
 
 # Connect to Wi-Fi
 wifi = network.WLAN(network.STA_IF)
@@ -47,28 +51,21 @@ wifi = network.WLAN(network.STA_IF)
 
 def connectToWifi():
     while not wifi.isconnected():
-        # print("Trying to establish Wi-Fi connection...")
         wifi.active(True)
         wifi.connect(ssid, password)
-        # print("Wait for next wifi connection try...")
         utime.sleep(10)
 
-    # Print the IP address assigned by the Wi-Fi network
     # print('Connected to Wi-Fi')
     # print('IP Address:', wifi.ifconfig()[0])
 
 
-# print("Calling wifi connection function")
 connectToWifi()
 
-# Small animation for debug LED to indicate that the Wi-Fi connection is established
-# print("Debug LED animation")
 while led_time > 0.1:
     utime.sleep(led_time)
     led_pin.toggle()
     led_time = led_time - 0.1
 
-# Turn off LED after animation
 led_pin.low()
 
 
@@ -90,17 +87,12 @@ def update_firestore(url, data):
     retries = 0
     while retries < 2:
         try:
-            # print("Trying to fetch the existing document for update (calling urequests.get)")
             response = urequests.get(url, headers=headers, timeout=10)
-            # print("Success, response is available")
             existing_data = response.json()
 
-            # Update the fields in the existing data
             for key, value in data.items():
                 existing_data['fields'][key] = {'booleanValue' if isinstance(value, bool) else 'integerValue': value}
 
-            # Update Firestore
-            # print("Trying to patch Firestore document with new data...")
             response = urequests.patch(url, data=ujson.dumps(existing_data), headers=headers, timeout=10)
             if response.status_code == 200:
                 # print("Data stored in Firestore successfully")
@@ -115,9 +107,8 @@ def update_firestore(url, data):
 
         retries += 1
         # print("Retrying... ({}/{})".format(retries, 2))
-        utime.sleep(0.1)  # Wait for 0.1 seconds before retrying
+        utime.sleep(0.1)
     # print("Exceeded maximum retries. Failed to store data in Firestore.")
-    # print("Calling connectToWifi()")
     connectToWifi()
 
 
@@ -131,80 +122,67 @@ def read_dht11():
 
 
 def control_ventilator(humidity, temperature):
-    global vent_on, override_on, max_humidity, max_temperature
+    global vent_on, override_on, max_humidity, max_temperature, min_humidity, min_temperature, start_time
 
-    # If override is on, use manual control
     if override_on:
-        # Set the vent pin value based on manual control
         vent_pin.value(1 if is_manual_vent_on else 0)
         vent_pin_n.value(1 if is_manual_vent_on else 0)
-
-
-        # Check the current state of the vent pin
         vent_on = vent_pin.value() == 1
-
-        # Update Firestore with the current state of the ventilator
         update_firestore(sensor_url, {'ventilatorOn': vent_on})
         return
 
-    # Automatic control based on thresholds
+    current_time = utime.ticks_ms()
+
     if humidity > max_humidity or temperature > max_temperature:
         vent_on = True
         vent_pin.value(1)
         vent_pin_n.value(1)
-        update_firestore(sensor_url, {'ventilatorOn': vent_on})
-    else:
+        start_time = current_time  # Reset timeout timer
+    elif (humidity <= min_humidity and temperature <= min_temperature) or (utime.ticks_diff(current_time, start_time) > timeout):
         vent_on = False
         vent_pin.value(0)
         vent_pin_n.value(0)
-        update_firestore(sensor_url, {'ventilatorOn': vent_on})
+
+    update_firestore(sensor_url, {'ventilatorOn': vent_on})
 
 
-# print("Entering infinity loop...")
 while True:
     try:
-        # print("Reading sensor data...")
-        # Fetch settings data from Firestore
         settings_data = get_firestore_data(settings_url)
         if settings_data:
             fields = settings_data.get("fields", {})
-            max_humidity = int(fields.get("humidity", {}).get("integerValue", max_humidity))
-            max_temperature = int(fields.get("temperature", {}).get("integerValue", max_temperature))
+            max_humidity = int(fields.get("maxHumidity", {}).get("integerValue", max_humidity))
+            min_humidity = int(fields.get("minHumidity", {}).get("integerValue", min_humidity))
+            max_temperature = int(fields.get("maxTemperature", {}).get("integerValue", max_temperature))
+            min_temperature = int(fields.get("minTemperature", {}).get("integerValue", min_temperature))
             override_on = bool(fields.get("overrideOn", {}).get("booleanValue", override_on))
             is_manual_vent_on = bool(fields.get("isManualVentOn", {}).get("booleanValue", is_manual_vent_on))
 
-        # Fetch control data from Firestore
         sensor_data = get_firestore_data(sensor_url)
         if sensor_data:
             fields = sensor_data.get("fields", {})
             vent_on = fields.get("ventilatorOn", {}).get("booleanValue", False)
 
-        # Read sensor data from DHT11
         current_humidity, current_temperature = read_dht11()
         if current_humidity is not None and current_temperature is not None:
             control_ventilator(current_humidity, current_temperature)
 
-            # Update Firestore with current sensor readings
             update_firestore(sensor_url, {
                 'humidity': current_humidity,
                 'temperature': current_temperature
             })
 
-        # Print current memory usage
         # print('Memory Allocation:', gc.mem_alloc(), 'bytes')
         # print('Memory Free:', gc.mem_free(), 'bytes')
-        # # Print IP address
         # print('IP Address:', wifi.ifconfig()[0])
 
-        # Toggle the debug LED to indicate Firestore write
         led_pin.toggle()
         utime.sleep(0.2)
         led_pin.toggle()
         utime.sleep(0.5)
 
     except Exception as e:
-        print("Exception occurred:", str(e))
-        print("Restarting Pico...")
+        # print("Exception occurred:", str(e))
+        # print("Restarting Pico...")
         utime.sleep(1.0)
         machine.reset()
-
